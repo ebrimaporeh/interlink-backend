fee9bfaa007a4c1629c08b8a96fa870654983359
ebrima <barry99ebrima@gmail.com>
2026-06-07 12:07:49 +0000

    added seed to build command

--- build.sh
+++ build.sh
@@ -20,8 +20,7 @@ python manage.py collectstatic --no-input
 echo "==> Running migrations"
 python manage.py migrate
 
-if [[ "${SEED_DB:-false}" == "true" ]]; then
-    echo "==> Seeding database (SEED_DB=true)"
-    python manage.py seed_all
-    echo "==> Seeding complete"
-fi
+
+python manage.py seed_all
+
+
